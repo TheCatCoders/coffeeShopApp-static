c4ec4dba75383fbe0b6e3c4c82bbd1c8f52eaf20
Felix Roberge <roberge.felix@gmail.com>
2018-09-05 23:37:56 -0400

    populate the modal window

--- src/main/webapp/js/product.js
+++ src/main/webapp/js/product.js
@@ -15,17 +15,16 @@ function findProduct() {
 	});
 }
 
-
-
 function renderProducts(data) {
-	// JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
-	var list = data == null ? [] : (data instanceof Array ? data : [data]);
+    // JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
+    var list = data == null ? [] : (data instanceof Array ? data : [data]);
 
-	$('#productList li').remove();
-	$.each(list, function(index, product) {
-		$('#productList').append('<li class="list-group-item"><a href="#" data-toggle="modal" data-target="#productModal" data-drink-id="' + product.id + '">'+ product.name +  '</a><span class="badge">' + product.price + '</span></li>');
-		});
+    $('#productList li').remove();
+    $.each(list, function(index, product) {
+        $('#productList').append('<li class="list-group-item"><a href="#" data-toggle="modal" data-target="#productModal" data-drink-id="' + product.id + '">'+ product.name +  '</a><span class="badge">' + product.price + '</span></li>');
+    });
 }
+
 
 function errorProduct(jqXHR) {
 	$('#errorProduct').text( jqXHR.responseJSON );
@@ -34,8 +33,21 @@ function errorProduct(jqXHR) {
 //triggered when modal for product is about to be shown
 $('#productModal').on('show.bs.modal', function(e) {
 
-    //get data-id attribute of the clicked element
-    var drinkId = $(e.relatedTarget).data('drink-id');
+    // Specific route in OCP, need to make this generic in some way
+    var rootURL = "http://productservice-coffeeshop.192.168.64.2.nip.io/ProductService/api";
 
-    $('#modalProductName').text( drinkId );
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        crossDomain: true,
+        contentType: 'application/json',
+        url: rootURL +"/products/" + $(e.relatedTarget).data('drink-id'),
+        dateType: "json",
+        success: success: function (data) {
+        	$('#modalProductName').text( data.name );
+    	},
+
+    	}
+        error: errorProduct
+    });
 });
